a52206aa355a8c12ed693d637c0cc435df7e18c8
Dean Baberowski <deanbaberows@gmail.com>
2025-11-04 11:34:34 +1100

    complete code along

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    
+    fib_list = []
+    a, b = 0, 1
+    while len(fib_list) < length:
+        fib_list.append(a)
+        a, b = b, a + b
+    print(fib_list)
